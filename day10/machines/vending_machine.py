8a02113360d7566525b3732be16c2529c7cfc6f7
딩코 <dingco@dingkoui-iMac.local>
2024-01-11 07:10:07 +0900

    day10
    Signed-off-by: 딩코 <dingco@dingkoui-iMac.local>

---
+++ day10/machines/vending_machine.py
@@ -0,0 +1,44 @@
+import emoji
+
+class VendingMachine:
+    def __init__(self, products):
+        self.products = products
+        self.balance = 0
+
+    def input_menu(self):
+        print("\n\n\n\n\n")
+        print(f"자판기 프로그램 {emoji.emojize(':grinning_face_with_big_eyes:')}")
+        print("-----------------------")
+        for idx in range(len(self.products)):
+            product = self.products[idx]
+            print(f"{idx + 1}. {product['name']}({product['count']}개) : {product['price']}원")
+        print("-----------------------")
+        print(f"현재 잔액 : {self.balance} 원")
+        print("-----------------------")
+        print("")
+        print("1. 상품 선택")
+        print("2. 돈 투입")
+        print("3. 잔액 반환")
+        print("4. 종료")
+        print("")
+        menu = int(input("메뉴 선택 : "))
+        return menu
+    
+    def insert_money(self):
+        money = int(input("투입 금액 입력 (원): "))
+        self.balance = self.balance + money
+        print(f">> {money}원이 투입되었습니다.")
+
+    def return_balance(self):
+        print(f">> {self.balance}원을 반환합니다.")
+        self.balance = 0
+
+    def select_product(self):
+        num = int(input("상품 번호 입력 : "))
+        product = self.products[num-1]
+        if product['count'] > 0 and self.balance >= product["price"]:
+            print(f">> {product['name']}를 구입 했습니다!")
+            self.balance = self.balance - product["price"]
+            product["count"] = product["count"] - 1
+        else:
+            print(f">> 구입할 수 없습니다.")
---
+++ day10/main.py
@@ -0,0 +1,24 @@
+import time
+from machines.vending_machine import VendingMachine
+
+vm = VendingMachine([
+    {"name": "콜라", "price": 2000, "count": 5},
+    {"name": "사이다", "price": 750, "count": 5},
+    {"name": "환타", "price": 1500, "count": 5}
+])
+
+
+while True:
+    menu = vm.input_menu()
+    if menu == 1:
+        vm.select_product()
+    elif menu == 2:
+        vm.insert_money()
+    elif menu == 3:
+        vm.return_balance()
+    elif menu == 4:
+        print("프로그램 종료")
+        break
+    else:
+        print(f"잘못된 메뉴 : {menu}")
+    time.sleep(2)
